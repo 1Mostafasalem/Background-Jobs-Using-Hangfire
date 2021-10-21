6dd0d8aab4ad021d467418c73055867953e47ac5
Mostafa Salem <mostafasalem858@outlook.com>
2021-10-21 05:32:21 +0200

    Add project files.

---
+++ BackgroundJobsUsingHangfire/Controllers/HangfireController.cs
@@ -0,0 +1,28 @@
+﻿using Hangfire;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackgroundJobsUsingHangfire.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HangfireController : ControllerBase
+    {
+        [HttpGet]
+        public void Get()
+        {
+            //Fire-and-forget :These jobs are executed only once and almost immediately after they are fired.
+            BackgroundJob.Enqueue(() => SendMessage("Hangfire@outlook.com"));
+        }
+
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public void SendMessage(string email)
+        {
+            Console.WriteLine($"Mail Sent at {DateTime.Now}");
+        }
+    }
+}
